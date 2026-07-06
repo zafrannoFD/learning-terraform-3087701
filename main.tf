631235650182ffd602ddd3d2f02b9c76f459ca2d
zafrannoFD <zafran_azhar@firstderivative.com>
2026-07-06 14:17:12 +0800

    Add SSH rule

--- main.tf
+++ main.tf
@@ -73,3 +73,13 @@ resource "aws_security_group_rule" "blog_everything_out" {
 
   security_group_id = aws_security_group.blog.id
 }
+
+resource "aws_security_group_rule" "blog_ssh_in" {
+  type        = "ingress"
+  from_port   = 22
+  to_port     = 22
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
