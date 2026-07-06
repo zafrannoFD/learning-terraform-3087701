5ea0b7c06e739d5ba8e3afc307222bd3a7d2173e
zafrannoFD <zafran_azhar@firstderivative.com>
2026-07-06 14:52:39 +0800

    Update ID

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.id]
 
   user_data = <<-EOF
     #!/bin/bash
